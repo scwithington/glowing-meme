4a4fc6dfd733ed75247b689f55959a37ed5d47c5
Cal Withington <scwithington@gmail.com>
2021-03-23 19:50:23 -0400

    question continuation, question reset, trouble with replacing answers, added timer and questionIndex

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,17 +6,51 @@ var startButton = document.getElementById('start-button');
 var startScreen = document.getElementById('start-screen');
 var questionScreen = document.getElementById('question-screen');
 
-var buttonOne = document.getElementById('buttonFirst');
-var buttonTwo = document.getElementById('buttonSecond');
-var buttonThree = document.getElementById('buttonThird');
-var buttonFour = document.getElementById('buttonFourth');
-var buttons = document.querySelectorAll('button');
+// Individual button variables
+// var buttonOne = document.getElementById('buttonFirst');
+// var buttonTwo = document.getElementById('buttonSecond');
+// var buttonThree = document.getElementById('buttonThird');
+// var buttonFour = document.getElementById('buttonFourth');
+// var buttons = document.querySelectorAll('button');
+
+var playerScore = 0;
+var currentQIndex = 0;
+
+var gameDuration = 90;
 
 startButton.addEventListener('click', startGame)
 
 function startGame(){
     startScreen.classList.add('hidden');
     questionScreen.classList.remove('hidden');
+    //start the timer
+    startTimer();
+    //initialize score to 0
+    displayQuestions();
+}
+
+function startTimer(){
+    //timer code
+    var timer = document.createElement("div");
+    timer.setAttribute("id", "timer-readout");
+    document.body.appendChild(timer);
+    timerInterval = setInterval(() => {
+        gameDuration--;
+        // Math variables stating a rotation of 60 seconds
+        var minuteHand = Math.floor(gameDuration / 60);
+        var secondHand = gameDuration % 60;
+        // Assigning an extra zero in front of minute-hand numbers that are less than 10.
+        if (secondHand < 10) {
+            secondHand = '0' + secondHand;
+        }
+        // 
+        timer.innerHTML = `${minuteHand}:${secondHand}`;
+        // Terminate game function when seconds less than or equal to zero
+        if (gameDurationInSec = 0) {
+            gameOver();
+        // console.log(gameDuration);
+        }
+    },   1000); // 1000 ms per interval
 }
 
 const myQuestions = [
@@ -49,20 +83,48 @@ const myQuestions = [
     }
 ];
 
+
 // console.log(myQuestions);
 
-// function checkAnswer(correctAnswer) {
-//     if(correct = true) {
-//         alert('correct');
-//     } else {
-//         alert('incorrect');
-//     }
+function displayQuestions() {
+    questionReset();
+    currentQIndex++;
+    nextQuestion();
+}
+
+function questionReset() {
+    
+}
+
+function nextQuestion(question) {
+    questionTitleEl.innerHTML = myQuestions[currentQIndex].question;
+    myQuestions[currentQIndex].answers.forEach((answer) => {
+        const button = document.createElement("button");
+        button.innerHTML = myQuestions[currentQIndex].answers[answer];
+        button.setAttribute("class", "choice");
+    // button.innerText = answers.text;
+        questionAnswerChoicesEl.appendChild(button);
+        button.addEventListener('click', function() {
+            displayQuestions();
+        });
+    });
+}
+
+// function userInput {
+
 // }
 
-// var buttons = addEventListener('click', function(){
-//     checkAnswer();
-//     nextQuestion();
-// });
+function checkAnswer(correctAnswer) {
+    if(answers.correct = true) {
+        alert('correct');
+    } else {
+        alert('incorrect');
+    }
+}
+
+
+
+console.log(buttons)
 
 // function nextQuestion() {
 //     questionTitleEl.textContent = myQuestions[0].question;
@@ -72,3 +134,15 @@ const myQuestions = [
 //     buttonFour.textContent = myQuestions[0].answers[3];
 // }
 
+function gameOver() {
+    if (gameDuration > 0) {
+        gameDuration = 0;
+        gameDuration.innerHTML = '0';
+    }
+
+    // return;
+}
+
+function gameReset() {
+
+}
